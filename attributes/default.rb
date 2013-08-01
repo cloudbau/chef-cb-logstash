fdf008ded403ba1a457278ca7388013c2e63c0ca
mouad benchchaoui <m.benchchaoui@cloudbau.de>
2013-08-01 17:18:49 +0200

    Fix checking for available openstack services by checking if the virtualenv exist instead of check if the node attribute exist which didn't work because even if a package is not scheduled to be installed there will be a reference of it in the node hash if it's a dependency of another one.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,6 @@ default["cb-logstash"]["logging"]["formatter"] = "git+git://github.com/mouadino/
 
 override['logstash']['elasticsearch_role'] = 'single-controller'
 override['logstash']['server']['enable_embedded_es'] = false
-override['logstash']['elasticsearch_role'] = 'single-controller'
 
 override['kibana']['branch'] = "4deca04e2571df3f05e52cb32fcd1cec74f907e1"
 override['kibana']['webserver_listen'] = "*"
--- recipes/agent.rb
+++ recipes/agent.rb
@@ -2,14 +2,14 @@ include_recipe "python::pip"
 include_recipe "logstash::agent"
 
 # Get all openstack services loaded by this node.
-node_services = node["cb-logstash"]["openstack"]["services"].select{ |s| node.has_key? s }
+node_services = node["cb-logstash"]["openstack"]["services"].select{ |s| File.exists?("/opt/cloudbau/#{s}-virtualenv") }
 
 # Get ip used by elasticsearch.
-node['logstash']['elasticsearch_ip'] = search(:node, "roles:logstash-server AND chef_environment:#{node.chef_environment}").first.ipaddress
-node['logstash']['elasticsearch_port'] = 9200  # XXX: Port is hard coded here.
+node.set['logstash']['elasticsearch_ip'] = search(:node, "roles:logstash-server AND chef_environment:#{node.chef_environment}").first.ipaddress
+node.set['logstash']['elasticsearch_port'] = 9200  # XXX: Port is hard coded here.
 
 node_services.each do |srv_name|
-  node['logstash']['agent']['inputs'].push({
+  node.set['logstash']['agent']['inputs'].push({
     'file' => {
       'type' => srv_name,
       'start_position' => 'beginning',
@@ -19,9 +19,10 @@ node_services.each do |srv_name|
   })
 end
 
-node['logstash']['agent']['outputs'].push({
+node.set['logstash']['agent']['outputs'].push({
   'elasticsearch_http' => {
-    'host' => "#{node['logstash']['elasticsearch_ip']}:#{node['logstash']['elasticsearch_port']}",
+    'host' => "#{node['logstash']['elasticsearch_ip']}",
+    'port' => "#{node['logstash']['elasticsearch_port']}",
     'flush_size' => 1
   }
 })
